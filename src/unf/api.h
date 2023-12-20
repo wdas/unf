c1aced460911b61476c17660ba4ee07dce647b33
mati-nvidia <mcodesal@nvidia.com>
2023-12-20 16:00:30 -0500

    Add full UNF_EXPORTS

--- src/unf/api.h
+++ src/unf/api.h
@@ -3,7 +3,22 @@
 
 #include "pxr/base/arch/export.h"
 
-#define UNF_API ARCH_EXPORT
-#define UNF_LOCAL ARCH_HIDDEN
+#if defined(UNF_STATIC)
+#   define UNF_API
+#   define UNF_API_TEMPLATE_CLASS(...)
+#   define UNF_API_TEMPLATE_STRUCT(...)
+#   define UNF_LOCAL
+#else
+#   if defined(UNF_EXPORTS)
+#       define UNF_API ARCH_EXPORT
+#       define UNF_API_TEMPLATE_CLASS(...) ARCH_EXPORT_TEMPLATE(class, __VA_ARGS__)
+#       define UNF_API_TEMPLATE_STRUCT(...) ARCH_EXPORT_TEMPLATE(struct, __VA_ARGS__)
+#   else
+#       define UNF_API ARCH_IMPORT
+#       define UNF_API_TEMPLATE_CLASS(...) ARCH_IMPORT_TEMPLATE(class, __VA_ARGS__)
+#       define UNF_API_TEMPLATE_STRUCT(...) ARCH_IMPORT_TEMPLATE(struct, __VA_ARGS__)
+#   endif
+#   define UNF_LOCAL ARCH_HIDDEN
+#endif
 
 #endif
