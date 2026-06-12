baefb44efb57ad3f600d03303b1310a31f6d12e6
Neil Barber <neil.barber@disneyanimation.com>
2026-06-12 13:26:59 -0700

    build: Mirgate the build script into it's own file
    Add build.sh and update the recipe to run it. This will give
    the ability to add logic at build time based off of the current
    environment.
    
    Signed-off-by: Neil Barber <neil.barber@disneyanimation.com>

---
+++ etc/roll/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+prefix="$1"
+destdir="$2"
+buildflavor="$3"
+numjobs="$4"
+
+mkdir -p build/$buildflavor &&
+cd build/$buildflavor &&
+cmake  \
+    -D CMAKE_INSTALL_PREFIX=$prefix \
+    -D GTest_ROOT=$RP_gtest \
+    -D TBB_INCLUDE_DIRS=$RP_TBB/include \
+    -D Boost_ROOT=$RP_boost_disney \
+    -D USD_INCLUDE_DIR=$RP_presto_usd/include \
+    ../.. &&
+make -j$numjobs &&
+make DESTDIR=$destdir install
