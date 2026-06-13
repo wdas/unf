67db287bfc42ac972af7d2243b13d3bc73fa6bde
Neil Barber <neil.barber@disneyanimation.com>
2026-06-12 23:26:49 -0700

    build.sh: Inject the path to the TBB's version header
    TBB versions are defined in different headers between classic TBB and
    OneTBB.  Add logic to set the correct path to the version header at
    build time.
    
    Signed-off-by: Neil Barber <neil.barber@disneyanimation.com>

--- etc/roll/build.sh
+++ etc/roll/build.sh
@@ -7,8 +7,10 @@ numjobs="$4"
 
 if test -n "$RP_oneTBB"; then
     tbbroot="$RP_oneTBB"
+    tbbversion_header="$tbbroot/include/oneapi/tbb/version.h"
 else
     tbbroot="$RP_TBB"
+    tbbversion_header="$tbbroot/include/tbb/tbb_stddef.h"
 fi
 
 mkdir -p build/$buildflavor &&
@@ -17,6 +19,7 @@ cmake  \
     -D CMAKE_INSTALL_PREFIX=$prefix \
     -D GTest_ROOT=$RP_gtest \
     -D TBB_INCLUDE_DIRS=$tbbroot/include \
+    -D TBB_VERSION_H=$tbbversion_header \
     -D USD_INCLUDE_DIR=$RP_presto_usd/include \
     ../.. &&
 make -j$numjobs &&
